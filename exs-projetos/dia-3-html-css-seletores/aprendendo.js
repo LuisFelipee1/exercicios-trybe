6183e75f6028f763b516fa641a502423645c3b14
LuisFelipee1 <luisfelipealves128@gmail.com>
2023-06-02 16:49:22 -0300

    commitando

--- exs-projetos/dia-3-html-css-seletores/aprendendo.js
+++ exs-projetos/dia-3-html-css-seletores/aprendendo.js
@@ -5,12 +5,14 @@ let somaNumeros = 0;
 //for (let index = 0; index < numbers.length; index += 1) {
 // console.log(numbers[index]);
 //} 
+
 // 2 - EXERCÍCIO FEITO!
 //for (let index = 0; index < numbers.length; index += 1) {
 //    somaNumeros += numbers[index]
 //}
 //console.log(somaNumeros);
 // 3 - EXERCÍCIO FEITO !
+
 //for (let index = 0; index < numbers.length; index += 1) {
 //   somaNumeros += numbers[index]
 //}
@@ -18,15 +20,55 @@ let somaNumeros = 0;
 //console.log(divisao);
 //OU console.log(somaNuumeros / 10);
 
-for (let index = 0; index < numbers.length; index += 0) { 
-    somaNumeros += numbers[index];
-};
+// 4 - EXERCICIOS FEITO!
+//for (let index = 0; index < numbers.length; index += 1) {
+//   somaNumeros += numbers[index]
+//}
 
-let divisao = somaNumeros / numbers.length;
+//let divisao = somaNumeros / numbers.length
 
 //if (divisao >= 20) {
 // console.log('O valor da média aritmética é maior que 20');
 //} else { 
 //console.log('O valor da média arítmética é menor que 20');
 //}
-console.log(divisao);
+//console.log(divisao);
+
+//5 - EXERCICIO FEITO!
+//let maiorNumero = 0;
+//for (let index = 0; index < numbers.length; index += 1) {
+//    somaNumeros += numbers[index]
+//    if (maiorNumero < numbers[index]) {
+//        maiorNumero = numbers[index]
+//    }
+//} 
+//console.log(maiorNumero);
+
+//let resultado = 0;
+
+//for (let index = 0; index < numbers.length; index += 1) {
+//}   if (numbers % 2 !== 0) {
+//       resultado += 1;
+//} 
+//if (resultado === 0) {
+//    console.log('Nenhum valor ímpar encontrado');
+//} else {
+//   console.log(resultado);
+//}
+
+
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    result += 1;
+  }
+};
+
+if (result === 0) {
+  console.log('Nenhum valor ímpar encontrado');
+} else {
+  console.log(result);
+};
+
+
---
+++ exs-projetos/dia-3-html-css-seletores/function.js
@@ -0,0 +1,7 @@
+function minhaFuncao (message) {
+    console.log(message);
+}
+
+minhaFuncao('ola miundo');
+minhaFuncao('Hellow');
+minhaFuncao(6);
